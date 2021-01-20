328d903660e257406f076ccbc8d49fddc7cc4a2b
Bryan Hildebrand <bryanhildebrand1@gmail.com>
2021-01-20 02:06:33 +0000

    added shell table and comments for project requirements

--- main.sql
+++ main.sql
@@ -1,2 +1,18 @@
 .header on
 .mode column
+
+
+
+CREATE TABLE interships
+(
+);
+
+
+
+/*
+	A table for the companies, to include the Name, location, business sector and contact information
+	A table for the internship positions, to include the position title, company name, application deadline, duration of the position, description of responsibilities and requirements, pay and any other pertinent information.
+	A way to list all the positions chronologically by application deadline
+	The ability to search by company for available internships.
+	Include one more query of your own design.
+*/
