28eee2bd7bd67a30b1eef1bdafda41929d706655
Bryan Hildebrand <bryanhildebrand1@gmail.com>
2021-01-25 06:56:26 +0000

    filled internships list

--- main.sql
+++ main.sql
@@ -1,25 +1,12 @@
 .header on
 .mode column
-
-
-
-CREATE TABLE companies
-(
-	companyName TEXT,
-	companyLocation TEXT,
-	bussinessSector TEXT,
-	contactInfo TEXT
-);
-CREATE VIEW internshipByDate as
-	SELECT * FROM internships ORDER BY deadline;
-DROP TABLE IF EXISTS internships;
-DROP TABLE IF EXISTS companies;
 .mode csv
 .import internships.csv internships
 .import companies.csv companies
-
-
-
+CREATE VIEW internshipByDate as
+	SELECT * FROM internships ORDER BY deadline;
+.mode column
+SELECT * FROM internshipByDate;
 
 /*INSERT INTO internships VALUES 
 	('test0','sample0', date('2021-12-21'),100,'sample0',1.0,'null'),
